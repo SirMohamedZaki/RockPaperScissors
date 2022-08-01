79a0973a82391e77c8cdf38fcd4d442d08af4995
SirMohamedZaki <moh.ashraf.zaki@gmail.com>
2022-08-01 09:40:04 +0200

    initial commit

--- script.js
+++ script.js
@@ -1,22 +1,73 @@
-const choices = [rock , paper , scissors];
+//rock paper scissors game
 
 function getComputerChoice() {
-    const randomNumber = Math.floor(Math.random() * choices.length);
+    const choices = ['rock', 'paper', 'scissors'];
+    const randomNumber = Math.floor(Math.random() * 3);
     return choices[randomNumber];
 }
 
-function singleRound (userChoice , computerChoice){
-    if (userChoice === computerChoice) {
-        return 'Draw';
-    }
-    if (userChoice === 'rock') {
-        return computerChoice === 'paper' ? 'Lose' : 'Win';
-    }
-    if (userChoice === 'paper') {
-        return computerChoice === 'scissors' ? 'Lose' : 'Win';
-    }
-    if (userChoice === 'scissors') {
-        return computerChoice === 'rock' ? 'Lose' : 'Win';
+const computerSelection = getComputerChoice();
+
+
+
+// Accessing the buttons and text labels
+const rockButton = document.getElementById("rock");
+const paperButton = document.querySelector("#paper");
+const scissorsButton = document.querySelector("#scissors");
+const playerScore = document.getElementById("player-score");
+const computerScore = document.getElementById("computer-score");
+const tieText = document.getElementById("result-tie");
+
+//Event listeners
+
+    rockButton.addEventListener('click', function() {
+        const playerSelection = 'rock';
+        const computerSelection = getComputerChoice();
+        const result = playRound(playerSelection, computerSelection);
+    });
+    paperButton.addEventListener('click', function() {
+        const playerSelection = 'paper';
+        const computerSelection = getComputerChoice();
+        const result = playRound(playerSelection, computerSelection);
+        
+    });
+    scissorsButton.addEventListener('click', function() {
+        const playerSelection = 'scissors';
+        const computerSelection = getComputerChoice();
+        const result = playRound(playerSelection, computerSelection);
+        
+    });
+
+//Game Logic
+
+function playRound(playerSelection , computerSelection) {
+
+    playerSelection = playerSelection.toLowerCase();
+    console.log(computerSelection);
+
+    if (playerSelection === computerSelection) {
+        return 'It is a tie!';
+    } else if (playerSelection === 'rock') {
+        if (computerSelection === 'paper') {
+            return 'You lose ! Rock beats Paper!';
+        } else {
+            return 'You win! Rock beats ' + computerSelection + '!';
+        }
+    } else if (playerSelection === 'paper') {
+        if (computerSelection === 'scissors') {
+            return 'You lose ! Paper beats Scissors!';
+        } else {
+            return 'You win! Paper beats ' + computerSelection + '!';
+        }
+    } else if (playerSelection === 'scissors') {
+        if (computerSelection === 'rock') {
+            return 'You lose! Rock beats Scissors!';
+        } else {
+            return 'You win! Scissors beats ' + computerSelection + '!';
+        }
     }
 }
 
+
+
+console.log(playRound(playerSelection,computerSelection));
